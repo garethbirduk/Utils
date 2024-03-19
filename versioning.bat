5141dce249dbde293f8782e3f20115897826f172
Gareth Bird <garethbird@gmail.com>
2024-03-19 22:48:17 +0000

    auto increment based on branch name

--- versioning.bat
+++ versioning.bat
@@ -1,4 +1,4 @@
 @echo off
 for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set "branch=%%i"
 echo Current branch: %branch%
-dotnet script .github/scripts/increment-version.csx %branch%
+dotnet script .github/scripts/update-version.csx %GITHUB_USERNAME% GradientSoftware.Utils %GITHUB_APIKEY% %branch%
