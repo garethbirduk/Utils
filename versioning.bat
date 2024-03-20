3a0d7f2ffd58af0fbe6b70f43ddcd1c0e5ee6ba4
garethbirduk <garethbird@gmail.com>
2024-03-20 10:26:36 +0000

    versioning (#18)

--- versioning.bat
+++ versioning.bat
@@ -1,4 +1,17 @@
 @echo off
+
+:: remove the cloned repository to ensure a clean start
+rd /s /q GradientSoftware.GithubScripts
+
+:: Get the current branch name
 for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set "branch=%%i"
 echo Current branch: %branch%
-dotnet script .github/scripts/update-version.csx %GITHUB_USERNAME% GradientSoftware.Utils %GITHUB_APIKEY% %branch%
+
+:: Clone the repository containing the script
+git clone --single-branch --branch main https://github.com/%GITHUB_USERNAME%/GradientSoftware.GithubScripts.git
+
+:: Run the script from the cloned repository
+dotnet script ./GradientSoftware.GithubScripts/.github/scripts/update-version.csx %GITHUB_USERNAME% GradientSoftware.Utils %GITHUB_APIKEY% .\Utils\Utils.csproj %branch%
+
+:: remove the cloned repository
+rd /s /q GradientSoftware.GithubScripts
