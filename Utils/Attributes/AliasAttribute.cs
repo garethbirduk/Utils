dcbc88c91b82fc7571087d2a1a57292f6ee269e4
garethbirduk <garethbird@gmail.com>
2024-04-01 00:16:32 +0100

    string to enum or default with alias support (#20)

---
+++ Utils/Attributes/AliasAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Utils.Attributes
+{
+    /// <summary>
+    /// Used to provide a string list of alias names for assisting in interrogation through Reflection.
+    /// </summary>
+    /// <remarks>
+    /// Constructor
+    /// </remarks>
+    /// <param name="list">The alias(es)</param>
+    [AttributeUsage(AttributeTargets.All, Inherited = false)]
+    public class AliasAttribute(params string[] list) : Attribute
+    {
+        /// <summary>
+        /// The list of aliases.
+        /// </summary>
+        public List<string> List { get; } = list.ToList();
+    }
+}
