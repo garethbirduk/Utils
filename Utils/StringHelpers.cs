6f22419f189d69dfdae2483b716f0b7e1c20598b
Gareth Bird <garethbird@gmail.com>
2024-06-13 12:35:15 +0100

    RandomName

---
+++ Utils/StringHelpers.cs
@@ -0,0 +1,19 @@
+﻿using PostSharp.Patterns.Contracts;
+
+namespace Gradient.Utils
+{
+    public static class StringHelpers
+    {
+        /// <summary>
+        /// Generate a random name based on a Guid
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="suffix"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string RandomName(string prefix = "", string suffix = "", [LessThan(37)] int length = 8)
+        {
+            return $"{prefix}{string.Join("", Guid.NewGuid().ToString().Take(length))}{suffix}";
+        }
+    }
+}
