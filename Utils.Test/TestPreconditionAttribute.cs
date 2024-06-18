abbca4787c1572caeb1b7f1b18d4aea8ac4f3ea2
garethbirduk <garethbird@gmail.com>
2024-06-18 22:03:05 +0100

    Precondtion Attribute (#30)

---
+++ Utils.Test/TestPreconditionAttribute.cs
@@ -0,0 +1,125 @@
+using Gradient.Utils.Attributes;
+using System.Linq.Dynamic.Core.Exceptions;
+
+namespace Gradient.Utils.Tests
+{
+    internal static class MyTestClass
+    {
+        [Precondition("myInt", "myInt > 10", "my error message")]
+        public static bool CustomErrorMessage(int myInt)
+        {
+            return myInt < 1000;
+        }
+
+        [Precondition("myInt", "myInt > 10")]
+        public static bool DefaultErrorMessage(int myInt)
+        {
+            return myInt < 1000;
+        }
+
+        [Precondition("myInt2", "null")]
+        public static bool GivesParseError(int myInt2)
+        {
+            return myInt2 < 50;
+        }
+
+        [Precondition("myInt", "myInt > 10")]
+        public static bool IsLessThan100(int myInt)
+        {
+            return myInt < 100;
+        }
+
+        [Precondition("myThing", "myThing != null && myThing.MyCount < 100")]
+        public static bool MyObjectMethod(Thing myThing)
+        {
+            return myThing is null;
+        }
+    }
+
+    [TestClass]
+    public class PreconditionAttributeTests
+    {
+        [TestMethod]
+        public void Test_Constructor()
+        {
+            Assert.IsNotNull(new PreconditionAttribute("a", "x != 1", "my message"));
+            Assert.IsNotNull(new PreconditionAttribute("a", "x != 1"));
+        }
+
+        [TestMethod]
+        public void Test_CustomErrorMessage()
+        {
+            var exceptionCaught = false;
+            try
+            {
+                MyTestClass.CustomErrorMessage(5);
+            }
+            catch (Exception ex)
+            {
+                exceptionCaught = true;
+                Assert.IsTrue(ex.Message.Contains("my error message"));
+            }
+            Assert.IsTrue(exceptionCaught);
+        }
+
+        [TestMethod]
+        public void Test_DefaultErrorMessage()
+        {
+            var exceptionCaught = false;
+            try
+            {
+                MyTestClass.DefaultErrorMessage(5);
+            }
+            catch (Exception ex)
+            {
+                exceptionCaught = true;
+                Assert.IsFalse(ex.Message.Contains("my error message"));
+                Assert.IsTrue(ex.Message.Contains("Parameter preconditions not met: myInt > 10"));
+            }
+            Assert.IsTrue(exceptionCaught);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_MyIntMethod_xTooLow()
+        {
+            MyTestClass.IsLessThan100(5);
+        }
+
+        [TestMethod]
+        public void TestIsNotNullExample_Error_IsNull()
+        {
+            Assert.ThrowsException<ArgumentException>(() => MyTestClass.MyObjectMethod(null));
+        }
+
+        [TestMethod]
+        public void TestIsNotNullExample_Error_MyCountTooHigh()
+        {
+            Assert.ThrowsException<ArgumentException>(() => MyTestClass.MyObjectMethod(new Thing() { MyCount = 999 }));
+        }
+
+        [TestMethod]
+        public void TestIsNotNullExample_NoError()
+        {
+            Assert.IsFalse(MyTestClass.MyObjectMethod(new Thing() { MyCount = 99 }));
+        }
+
+        [TestMethod]
+        public void TestIsNotNullExample_ParseError()
+        {
+            Assert.ThrowsException<ParseException>(() => MyTestClass.GivesParseError(25));
+        }
+
+        [TestMethod]
+        public void TestMyIntMethod_NoError()
+        {
+            Assert.IsTrue(MyTestClass.IsLessThan100(55));
+            Assert.IsFalse(MyTestClass.IsLessThan100(555));
+        }
+    }
+
+    public class Thing
+    {
+        public int MyCount { get; set; } = 99;
+    }
+}
---
+++ Utils/Attributes/PreconditionAttribute.cs
@@ -0,0 +1,49 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Gradient.Utils.Attributes
+{
+    [PSerializable]
+    public class PreconditionAttribute : OnMethodBoundaryAspect
+    {
+        private string _errorMessage;
+        private string _expression;
+        private string _parameterName;
+
+        public PreconditionAttribute(string parameterName, string expression, string? errorMessage = null)
+        {
+            _parameterName = parameterName;
+            _expression = expression;
+            _errorMessage = errorMessage ?? $"Parameter preconditions not met: {expression}";
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            var method = args.Method as MethodInfo;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            var parameters = method.GetParameters();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var parameter = parameters[i];
+                if (parameter.Name != _parameterName) continue; // Only validate the specified parameter
+
+                var value = args.Arguments[i];
+                var parameterType = parameter.ParameterType;
+                var lambda = DynamicExpressionParser.ParseLambda(new[] { Expression.Parameter(parameterType, parameter.Name) }, typeof(bool), _expression);
+                var compiledLambda = lambda.Compile();
+                var invokePrecondition = compiledLambda.DynamicInvoke(value);
+#pragma warning disable CS8605 // Unboxing a possibly null value.
+                if (!(bool)invokePrecondition)
+                {
+                    throw new ArgumentException(_errorMessage, parameter.Name);
+                }
+#pragma warning restore CS8605 // Unboxing a possibly null value.
+            }
+        }
+    }
+}
